5e45012880577f7efae42b74148d64d58914de7c
simonj <78244639+simonj-entry@users.noreply.github.com>
2021-02-08 20:14:59 +0900

    Update like.js

--- like.js
+++ like.js
@@ -1,2 +1,2 @@
-import {l, url, e, pj, ds, my, users} from "https://raw.githubusercontent.com/ddtt786GH/eapi/main/code/index.js"
+$.get('https://raw.githack.com/ddtt786GH/eapi/main/code/index.js');
 pj('600797be5dc14c0372000e13').like()
